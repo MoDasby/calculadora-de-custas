22588974212414088db44afd903f7c5597959f9a
MoDasby <giullianomendes033@gmail.com>
2024-11-28 10:17:52 -0300

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/MoDasby/calculadora-de-custas"
+  base: "https://modasby.github.io/calculadora-de-custas/"
 })
